f3949099e831d30b19f72a4daaa4545da98758e2
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2025-01-23 16:30:09 +0600

    removing fix server

--- fixing_server/routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const apiController = require('../controllers/apiController');
-const router = express.Router();
-
-router.post('/upload-source-code', apiController.getUploadSourceCode);
-router.post('/fix-leak', apiController.fixLeak);
-
-module.exports = router;
